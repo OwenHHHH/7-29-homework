ed3e4f1e9840ef0e46116848bb8b4956a49086da
OwenHHHH <owenhandelman@gmail.com>
2019-07-30 13:53:31 -0500

    k

--- 7-29-homework/src/notmainfunctionfile.kt
+++ 7-29-homework/src/notmainfunctionfile.kt
@@ -1,5 +1,5 @@
-fun toDouble (x: RationalNumber) {
-    return(x.numerator/x.denominator)
+fun toDouble (w: RationalNumber) {
+    return(w.numerator/w.denominator)
 }
 
 fun add (x: RationalNumber, y: RationalNumber){
@@ -7,3 +7,5 @@ fun add (x: RationalNumber, y: RationalNumber){
     return(((x.numerator*y.denominator)+(y.numerator*x.denominator))/(x.denominator*y.denominator))
 }
 
+//can you explain why this doesn't work or what the code should be?
+//because I don't get why this doesn't work
